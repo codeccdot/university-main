c7a97a80c1fb1b007822e71fbb7fc947a48aa96f
jason <949582632@qq.com>
2021-09-15 22:42:40 +0800

    制作页头Header

---
+++ hotel/src/components/index.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="index">
+<Footer></Footer>
+<Swiper></Swiper>
+</div>
+</template>
+
+<script>
+import Footer from '@/components/Footer'
+import Swiper from '@/components/swiper'
+export default {
+  name: "index",
+components:{
+    Footer,Swiper
+}
+,
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#index{
+
+
+.footer{
+
+}
+}
+
+</style>
---
+++ hotel/src/components/swiper.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="swiper" >
+  <v-carousel :show-arrows="false" style="width: 80vw ;height: 400px">
+    <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
+
+
+
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "swiper"
+  ,data(){
+    return {
+      items: [
+        {
+          src: require('@/assets/swiper1.jpg')
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+
+
+    }}
+
+}
+</script>
+
+<style  lang="scss" scoped>
+.swiper{
+  display: flex;
+  justify-content: center;position: relative;
+  top: 3px;
+
+
+}
+
+</style>
